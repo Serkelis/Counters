e0982d45062dd26ccb4aae03ab5510bf460087b9
skeliotis <skeliotis@bethel.jw.org>
2020-04-28 08:19:55 -0400

    added counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,33 +2,38 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ['tag1', 'tag2', 'tag3']
+  };
+
+  handleIncrement = (product) => {
+    console.log('ncrement clicked', this);
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className='btn btn-secondary btn-sm'>Increment</button>
-        <ul>
-          {this.state.tags.map((aTag) => (
-            <li key={aTag}>{aTag}</li>
-          ))}
-        </ul>
+        <button
+          className='btn btn-secondary btn-sm'
+          onClick={() => this.handleIncrement({ id: 1 })}
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
-  getBadgeClasses() {
-    let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
-    return classes;
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
+    return classes;
   }
 }
 
