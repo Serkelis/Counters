c692209457024267df21ee150f3d8a20ea53ccee
skeliotis <skeliotis@bethel.jw.org>
2020-05-02 20:21:03 -0400

    before introducing stateless

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    tags: ['tag1', 'tag2', 'tag3']
+    value: this.props.counter.value
   };
 
   handleIncrement = (product) => {
@@ -20,6 +19,12 @@ class Counter extends Component {
           onClick={() => this.handleIncrement({ id: 1 })}
         >
           Increment
+        </button>
+        <button
+          className='btn btn-danger btn-sm m-2'
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
         </button>
       </div>
     );
