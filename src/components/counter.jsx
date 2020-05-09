bfded31ee592e390c13fa66c482dccf7dab4b082
skeliotis <skeliotis@bethel.jw.org>
2020-05-09 15:19:42 -0400

    Final Version of Counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
-  handleIncrement = (product) => {
-    console.log('ncrement clicked', this);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           className='btn btn-secondary btn-sm'
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
         >
           Increment
         </button>
@@ -31,13 +22,13 @@ class Counter extends Component {
   }
 
   formatCount() {
-    const { value: count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
   }
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.value === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 }
